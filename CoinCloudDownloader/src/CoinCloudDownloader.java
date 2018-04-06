ade562e46317d747a729180393a416f28b005918
CoinCloudGuy <coincloudguy@zeroid.bit>
2018-04-06 21:58:04 +0200

    Cleanup, removed the need for a thread, fixed info box and Tor check.

--- CoinCloudDownloader/src/CoinCloudDownloader.java
+++ CoinCloudDownloader/src/CoinCloudDownloader.java
@@ -1,6 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -30,12 +32,23 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import javax.swing.SwingWorker;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.text.AbstractDocument;
 import javax.swing.text.AttributeSet;
+import javax.swing.text.BoxView;
+import javax.swing.text.ComponentView;
+import javax.swing.text.Element;
+import javax.swing.text.IconView;
+import javax.swing.text.LabelView;
+import javax.swing.text.ParagraphView;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
+import javax.swing.text.StyledEditorKit;
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
 
 public class CoinCloudDownloader extends JFrame {
 	private static final long serialVersionUID = -7471111582156575408L;
@@ -44,8 +57,7 @@ public class CoinCloudDownloader extends JFrame {
 	private JTextField textField_tx;
 	private JTextField textField_path;
 	private JTextPane textPane;
-	private volatile Boolean isBuisy;
-	public final long startTime;
+	private static boolean canUseTor;
 	public static final int ERROR = 0;
 	public static final int INFO = 1;
 	public static final int WARNING = 2;
@@ -53,18 +65,19 @@ public class CoinCloudDownloader extends JFrame {
 	public static final Color INFOColor = new Color(0, 0, 0);
 	public static final Color WARNINGColor = new Color(155, 155, 0);
 	public static final Color POINTYColor = new Color(0, 255, 0);
-	public static final String version = "0.0.4-Beta";
+	public static final String version = "0.0.5-Beta";
 	public static final String name = "CoinCloudDownloader";
 	
 	public static final String BLOCKCHAIN_Host = "blockchain.info";
 	public static final String BLOCKCHAIN_HostTor = "blockchainbdgpzk.onion";
-	public static final String URL_CheckTorConnection = "https://ipinfo.io/ip";//would normally just return the current public ip-address
-	public static final int TIMEOUT_CheckTorConnection = 20;//in seconds
 	public static final Proxy TORProxy = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("127.0.0.1", 9150));
+	public static final String TORProxyString = "127.0.0.1:9150";
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				//check for an running tor proxy
+				canUseTor = isTorProxyAvailable();
 				try {
 					CoinCloudDownloader frame = new CoinCloudDownloader();
 					frame.setVisible(true);
@@ -77,8 +90,6 @@ public class CoinCloudDownloader extends JFrame {
 	
 	
 	public CoinCloudDownloader() {
-		this.startTime = System.currentTimeMillis();
-		this.isBuisy = false;
 		setTitle(name + " " + version);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -101,7 +112,7 @@ public class CoinCloudDownloader extends JFrame {
 		
 		JCheckBox chckbxUseTor = new JCheckBox("Use Tor");
 		chckbxUseTor.setToolTipText("If enabled, the hidden service \"" + BLOCKCHAIN_HostTor + "\" will be used, in order to download the transaction. You need to have an Tor proxy installed and running on 127.0.0.1:9150 in order for this to work! Otherwise \"" + BLOCKCHAIN_Host + "\" will be used.");
-		chckbxUseTor.setSelected(true);
+		chckbxUseTor.setSelected(canUseTor);
 		panel.add(chckbxUseTor, BorderLayout.SOUTH);
 		
 		JPanel panel_1 = new JPanel();
@@ -147,41 +158,32 @@ public class CoinCloudDownloader extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				//this ensures that the button is not being spamed, since every press creates an new thread. Only one download at a time.
-				//Necessary, because swing doesn't handle multiple threads well (and comes up with a lot of surprises if one is not careful enough). Therefore JButton.setEnabled(false) could be dangerous.
-				synchronized (isBuisy) {
-					if(isBuisy.booleanValue())
-						return;
-					else
-						isBuisy = new Boolean(true);
-				}
+				btnNewButton.setEnabled(false);
 				//this ensures the the field-data is not being manipulated while the download is running
 				final String transaction = textField_tx.getText();
 				final boolean isCheckBoxTorChecked = chckbxUseTor.isSelected();
 				final String outputPath =textField_path.getText();
-				//new thread keeps the UI being responsive and the log screen updating.
-				Thread t = new Thread(new Runnable() {
+				//using a worker keeps the UI being responsive and the log screen updating.
+				SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>(){
 					@Override
-					public void run() {
+					protected Void doInBackground() throws Exception {
 						if(transaction.equals("")) {
 							log("The field transaction name is empty.",ERROR);
-							synchronized (isBuisy) {isBuisy = new Boolean(false);}
-							return;
+							return null;
 						}
 						if(!transaction.matches("-?[0-9a-fA-F]+")) {
 							log("The given transaction hash is not in hexadecimal, the transaction has to be in hexadecimal...",ERROR);
-							synchronized (isBuisy) {isBuisy = new Boolean(false);}
-							return;
+							return null;
 						}
 						boolean useTor = false;
 						if (isCheckBoxTorChecked) {
 							log("Use Tor is checked, testing proxy...", INFO);
-							if(isProxyAvailable(TORProxy)) {
+							if(isTorProxyAvailable()) {
 								useTor = true;
 								log("Tor is working.", INFO);
 							}else {
 								log("It has been selected that Tor should be used in the connection to " + BLOCKCHAIN_Host + "(" + BLOCKCHAIN_HostTor + "), but the Tor-proxy could not achieve an Internet connection. Make sure it runs under 127.0.0.1 on port 9150!", ERROR);
-								synchronized (isBuisy) {isBuisy = new Boolean(false);}
-								return;
+								return null;
 							}
 							log("Receiving transmission... Since an hidden service is used, this could take some time.", INFO);
 						}else {
@@ -192,22 +194,19 @@ public class CoinCloudDownloader extends JFrame {
 							hex = obtainHexFromTransaction(transaction, useTor);
 						} catch (IOException e2) {
 							log("There have been problems retrieving the transaction from " + (useTor? BLOCKCHAIN_HostTor : BLOCKCHAIN_Host) + "/rawtx/[...] maybe you have no Internet connection or the webside is down?",ERROR);
-							synchronized (isBuisy) {isBuisy = new Boolean(false);}
-							return;
+							return null;
 						}
 						log("The transaction has been received.", INFO);
 						if(outputPath == "") {
 							log("The path is empty!",ERROR);
-							synchronized (isBuisy) {isBuisy = new Boolean(false);}
-							return;
+							return null;
 						}
 						FileOutputStream fos;
 						try {
 							fos = new FileOutputStream(outputPath);
 						} catch (FileNotFoundException e2) {
 							log("The path is invalid!",ERROR);
-							synchronized (isBuisy) {isBuisy = new Boolean(false);}
-							return;
+							return null;
 						}
 						log("Writing data to file[" + outputPath + "]...", INFO);
 						List<Byte> b = new ArrayList<Byte>();
@@ -223,14 +222,18 @@ public class CoinCloudDownloader extends JFrame {
 							fos.close();
 						} catch (IOException e1) {
 							log("An exception while writing to the file, maybe not enough file permissions?", ERROR);
-							synchronized (isBuisy) {isBuisy = new Boolean(false);}
-							return;
+							return null;
 						}
 						log("DONE", INFO);
-						synchronized (isBuisy) {isBuisy = new Boolean(false);}
+						return null;
 					}
-				});
-				t.start();
+					
+					@Override
+					protected void done() {
+						btnNewButton.setEnabled(true);
+					}
+				};
+				worker.execute();
 			}
 		});
 
@@ -238,12 +241,12 @@ public class CoinCloudDownloader extends JFrame {
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBorder(null);
+		scrollPane.setViewportBorder(new LineBorder(new Color(192, 192, 192), 2, true));
 		panel_1.add(scrollPane, BorderLayout.CENTER);
 		
 		textPane = new JTextPane();
+		textPane.setEditorKit(new WrapEditorKit());
 		appendToLog(">", POINTYColor);
-		textPane.setFocusable(false);
-		textPane.setBorder(new LineBorder(new Color(192, 192, 192), 2, true));
 		textPane.setEditable(false);
 		scrollPane.setViewportView(textPane);
 		
@@ -259,6 +262,13 @@ public class CoinCloudDownloader extends JFrame {
 			}
 		});
 		panel_1.add(btnLookhere, BorderLayout.SOUTH);
+		
+		//set position
+		Dimension winDim = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation(
+				(winDim.width-getWidth())/2, //center x
+				(winDim.height-getHeight())/2); //center y
+		setMinimumSize(getSize());
 	}
 
 	public static void putToBytes(String s, List<Byte> l) {
@@ -372,16 +382,65 @@ public class CoinCloudDownloader extends JFrame {
 	        textPane.setEditable(false);
     }
     
-	private static boolean isProxyAvailable(Proxy p) {
-		HttpURLConnection con;
+	private static boolean isTorProxyAvailable() {
 		try {
-			con = (HttpURLConnection) new URL(URL_CheckTorConnection).openConnection(p);
+			HttpURLConnection con = (HttpURLConnection) new URL("http://" + TORProxyString).openConnection();
 			con.setRequestMethod("HEAD");
-			con.setConnectTimeout(TIMEOUT_CheckTorConnection*1000);
 			con.connect();
-			return con.getResponseCode()/100 == 2;//code 2xx means OK
+			return con.getResponseCode()!=-1;//-1 means invalid HTTP response
 		} catch (IOException e) {
 			return false;
 		}
 	}
+	
+	//fix for weird JTextPane (infoBox) wrap behavior
+    class WrapEditorKit extends StyledEditorKit {
+		private static final long serialVersionUID = 8085257318132218646L;
+		ViewFactory defaultFactory=new WrapColumnFactory();
+        public ViewFactory getViewFactory() {
+            return defaultFactory;
+        }
+    }
+	
+	class WrapColumnFactory implements ViewFactory  {
+        @Override
+        public View create(Element elem) {
+            String kind = elem.getName();
+            if (kind != null) {
+                if (kind.equals(AbstractDocument.ContentElementName)) {
+                    return new WrapLabelView(elem);
+                } else if (kind.equals(AbstractDocument.ParagraphElementName)) {
+                    return new ParagraphView(elem);
+                } else if (kind.equals(AbstractDocument.SectionElementName)) {
+                    return new BoxView(elem, View.Y_AXIS);
+                } else if (kind.equals(StyleConstants.ComponentElementName)) {
+                    return new ComponentView(elem);
+                } else if (kind.equals(StyleConstants.IconElementName)) {
+                    return new IconView(elem);
+                }
+            }
+            // default to text display
+            return new LabelView(elem);
+        }
+    }
+
+    class WrapLabelView extends LabelView {
+
+        public WrapLabelView(Element elem) {
+            super(elem);
+        }
+
+        @Override
+        public float getMinimumSpan(int axis) {
+            switch (axis) {
+                case View.X_AXIS:
+                    return 0;
+                case View.Y_AXIS:
+                    return super.getMinimumSpan(axis);
+                default:
+                    throw new IllegalArgumentException("Invalid axis: " + axis);
+            }
+        }
+    }
+	
 }
